0e084c9ac68d6d49eb62620624307ed5cfd9c228
Deep-Link-0721 <wisdom5necessary@gmail.com>
2025-06-13 00:23:16 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React, { useState } from 'react';
+import Papa from 'papaparse';
+import { CSVLink } from 'react-csv';
+import { Line } from 'react-chartjs-2';
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Amazon広告分析アプリ</h1>
+    </div>
+  );
+}
